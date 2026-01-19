3463297b9b8e4716a03ebc4e50e89742ef370d8f
Rafael Kanashiro <79936454+rafaelkanashiro@users.noreply.github.com>
2026-01-19 14:37:19 -0300

    Classe Campo atualizada e finalizada

--- src/br/com/rafaelkanashiro/cm/modelo/Campo.java
+++ src/br/com/rafaelkanashiro/cm/modelo/Campo.java
@@ -65,4 +65,59 @@ public class Campo {
 	boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
+
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	void minar() {
+		minado = true;
+	}
+	
+	public boolean isAberto() {
+		return aberto;
+	}
+	
+	public boolean isFechado() {
+		return !isAberto();
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	public boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		
+		return desvendado || protegido;
+	}
+	
+	long minasNaVizinhanca(){
+		return vizinhos.stream().filter(v -> v.minado).count();
+	}
+	
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
+	
+	public String toString() {
+		if(marcado) {
+			return "x";
+		}else if (aberto && minado) {
+			return "*";
+		}else if (aberto && minasNaVizinhanca() > 0) {
+			return Long.toString(minasNaVizinhanca());
+		}else if (aberto) {
+			return " ";
+		}else {
+			return "?";
+		}
+	}
 }
--- test/br/com/rafaelkanashiro/cm/modelo/CampoTeste.java
+++ test/br/com/rafaelkanashiro/cm/modelo/CampoTeste.java
@@ -1,9 +1,13 @@
 package br.com.rafaelkanashiro.cm.modelo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import br.com.rafaelkanashiro.cm.excecao.ExplosaoException;
 
 class CampoTeste {
 
@@ -20,5 +24,88 @@ class CampoTeste {
 		boolean campoVizinhoVerdadeiro = campo.adicionarVizinho(vizinho);
 		assertTrue(campoVizinhoVerdadeiro);
 	}
-
+	
+	@Test
+	void testeValidarCampoVizinhoDiagonal() {
+		Campo vizinho = new Campo(4,4);
+		boolean campoVizinhoVerdadeiro = campo.adicionarVizinho(vizinho);
+		assertTrue(campoVizinhoVerdadeiro);
+	}
+	
+	@Test
+	void testeValidarNaoVizinho() {
+		Campo vizinho = new Campo(5,5);
+		boolean campoVizinhoVerdadeiro = campo.adicionarVizinho(vizinho);
+		assertFalse(campoVizinhoVerdadeiro);
+	}
+	
+	@Test
+	void testeValorPadraoMarcacao() {
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacaoValorPadrao() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeNaoMarcadoNaoMinado() {
+		assertTrue(campo.abrir());
+	}
+	
+	@Test
+	void testeMarcadoNaoMinado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeMarcadoMinado() {
+		campo.minar();
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeNaoMarcadoMinado() {
+		campo.minar();
+		
+		assertThrows(ExplosaoException.class, () -> campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirVizinhos() {
+		Campo campo1 = new Campo(1,1);
+		Campo campo2 = new Campo(2,2);
+		
+		campo2.adicionarVizinho(campo1);
+		campo.adicionarVizinho(campo2);
+		
+		campo.abrir();
+		
+		assertTrue(campo1.isAberto() && campo2.isAberto());
+	}
+	
+	@Test
+	void testeAbrirVizinhosUmMinado() {
+		Campo campo1 = new Campo(1,1);
+		Campo campo2 = new Campo(2,2);
+		campo1.minar();
+		
+		campo2.adicionarVizinho(campo1);
+		campo.adicionarVizinho(campo2);
+		
+		campo.abrir();
+		
+		assertTrue(campo1.isFechado() && campo2.isAberto());
+	}
 }
