217525de3441532d10066fcccf4570e33aa2e200
Rafael Kanashiro <79936454+rafaelkanashiro@users.noreply.github.com>
2026-01-20 16:06:42 -0300

    Adicionados os atributos e métodos da classe Tabuleiro

--- src/br/com/rafaelkanashiro/cm/modelo/Tabuleiro.java
+++ src/br/com/rafaelkanashiro/cm/modelo/Tabuleiro.java
@@ -1,5 +1,42 @@
 package br.com.rafaelkanashiro.cm.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Tabuleiro {
+	private int colunas;
+	private int linhas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int colunas, int linhas, int minas) {
+		this.colunas = colunas;
+		this.linhas = linhas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+	
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+	}
+
+	private void associarVizinhos() {
+		for (Campo campo1 : campos) {
+			for (Campo campo2 : campos) {
+				campo1.adicionarVizinho(campo2);
+			}
+		}
+	}
+	
+	private void sortearMinas() {
+	}
 }
