24e1170ed969880e8b1bad3010bf1f6881977a8f
Rafael Kanashiro <79936454+rafaelkanashiro@users.noreply.github.com>
2026-01-21 13:53:05 -0300

    Adicionado métodos abrir e alternarMarcacao da classe Tabuleiro

--- src/br/com/rafaelkanashiro/cm/modelo/Tabuleiro.java
+++ src/br/com/rafaelkanashiro/cm/modelo/Tabuleiro.java
@@ -21,6 +21,16 @@ public class Tabuleiro {
 		sortearMinas();
 	}
 	
+	public void abrir(int linha, int coluna) {
+		campos.stream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+		.findFirst().ifPresent(Campo::abrir);
+	}
+	
+	public void alternarMarcacao(int linha, int coluna) {
+		campos.stream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+		.findFirst().ifPresent(Campo::alternarMarcacao);
+	}
+	
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
 			for (int coluna = 0; coluna < colunas; coluna++) {
