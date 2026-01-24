062034917b5bc7954636d7081da31f6566419ab2
Rafael Kanashiro <79936454+rafaelkanashiro@users.noreply.github.com>
2026-01-24 17:06:12 -0300

    Tamanho do tabuleiro da aplicação atualizado para 12x12 e 25 minas

--- src/br/com/rafaelkanashiro/cm/Aplicacao.java
+++ src/br/com/rafaelkanashiro/cm/Aplicacao.java
@@ -7,7 +7,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+		Tabuleiro tabuleiro = new Tabuleiro(12, 12, 25);
 		new TabuleiroConsole(tabuleiro);
 	}
 }
